b5e46bb0c66a883aad72db16938a5035bae09dd7
Ubuntu <smitp8720@gmail.com>
2020-10-25 17:13:16 -0700

    Part 2 Complete

--- test/tests.py
+++ test/tests.py
@@ -15,8 +15,8 @@
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
 tests = [ {'description': 'This test will run first.',
-    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 1 } ],
-    'expected': [('PORTB',0x02)],
+    'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 1 } ],
+    'expected': [('PORTC',0x00)],
     },
 #    {'description': 'This test will run second.',
 #    'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
@@ -30,5 +30,5 @@ tests = [ {'description': 'This test will run first.',
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 
 # variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['main::state','main::button','PORTB']
+watch = ['main::state','main::button0','main::button1','PORTC']
 
